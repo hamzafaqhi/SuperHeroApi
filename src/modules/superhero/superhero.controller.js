4567641b2bb100c293503a7b52c9a72d75ca5034
Hamza Faqhi <Hamzafaqhi@gmail.com>
2025-02-02 05:59:46 +0100

    added readme.md and comments

--- src/modules/superhero/superhero.controller.js
+++ src/modules/superhero/superhero.controller.js
@@ -1,27 +1,37 @@
 import {
   successResponse,
   errorResponse,
-} from "../../helpers/response.helper.js";
+} from "../../helpers/response.helper.js"; // Import helper functions to handle success and error responses
 import * as superHeroService from "./superhero.service.js";
 import { getSocketInstance } from "../../connectors/socket.js";
-
+// Controller function to handle retrieving all superheroes
 export const index = (req, res, next) => {
   try {
     const response = superHeroService.all();
     return successResponse(req, res, response);
   } catch (error) {
+    // If there is an error, return an error response with the error message
     return errorResponse(req, res, error.message, error?.code);
   }
 };
 
+// Controller function to handle creating a new superhero
 export const create = (req, res) => {
   try {
+    // Get the superhero data from the request body
     const payload = req.body;
+
     const response = superHeroService.create(payload);
-    const io = getSocketInstance(); // Retrieve the Socket.io instance
+
+    // Retrieve the Socket.io instance to communicate with the clients
+    const io = getSocketInstance();
+
+    // Emit an event to all connected clients notifying them of a new superhero
     io.emit("newSuperhero", response);
+
     return successResponse(req, res, response);
   } catch (error) {
+    // If there is an error, return an error response with the error message
     return errorResponse(req, res, error.message, error?.code);
   }
 };
--- src/modules/superhero/superhero.service.js
+++ src/modules/superhero/superhero.service.js
@@ -1,10 +1,13 @@
+// Array to hold the superheroes data
 let superheroes = [];
 
 export const create = (data) => {
+  // Add the new superhero data to the superheroes array
   superheroes.push(data);
   return superheroes;
 };
 
 export const all = () => {
+  // Sort superheroes array in descending order based on humility_score
   return superheroes.sort((a, b) => b.humility_score - a.humility_score);
 };
